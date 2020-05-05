faa0205dc5a66fb8130fd47b9c4e61bad724307f
Mark Start <markstart00@gmail.com>
2020-05-05 11:46:26 +0100

    extends fallingColorDropsWithControls

---
+++ app/ControlDrop.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.awt.Color;
+import app.ColorDrop;
+
+public class ControlDrop extends ColorDrop {
+	private float modifier = 1.0f;
+
+	public ControlDrop(int x, int y, int vx, int vy, int sz, Color c) {
+		super(x,y,vx,vy,sz,c);
+	}
+
+	public void move(int width, int height){
+		xpos = Math.round(xpos + getModifier() * xvel);
+		ypos = Math.round(ypos + getModifier() * yvel);
+
+		if (ypos > height) {
+			ypos = 0;
+			xpos = (int) (Math.random() * width);
+		}
+	}
+
+	public void setModifier(float modifier) {
+		this.modifier = modifier;
+	}
+
+	public float getModifier() {
+		return modifier;
+	}
+}
+
+
